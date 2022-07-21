d25af913785565aa3e0367de907357437a84d0ec
naveen3124 <naveenvit3124@gmail.com>
2022-07-21 21:27:15 +0530

    Create buy_and_sell_share.java

---
+++ buy_and_sell_share.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n== 0)
+            return 0;
+        int[][] dp = new int[k+1][n];
+        
+        for (int i =0; i<=k; i++) {
+            for(int j=0; j<n; j++) {
+                dp[i][j] = 0;
+            }
+        }
+   
+        // if no stock, no profit;
+        for (int i=0; i<n; i++) {
+            dp[0][i] = 0;
+        }
+        // if no days , then no need to buy
+        for (int i=0; i <= k; i++) {
+            dp[i][0] = 0;
+        }
+        for ( int i=1; i <=k ; i++) {
+            int bestBuy = 0 - prices[0]; // losing money so negative!!
+            for ( int j=1; j <n; j++) {
+                int DontbuyToday = dp[i][j-1]; 
+                int buyTodayAndProfit = bestBuy /* previous buy and sell transaction + best buy */ +
+                                        prices[j] /* buy today */;
+                dp[i][j] = Math.max(DontbuyToday, buyTodayAndProfit);
+                if (bestBuy < dp[i-1][j-1] - prices[j]) // profit i got in previous transaction (if done) -  current selling price gives the best cost price so far 
+                    bestBuy = dp[i-1][j-1] - prices[j];                    
+                
+            }            
+        }
+        return dp[k][n-1];
+    }
+}
